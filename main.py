bafbac140dde6e1a31b7dceaef06b7dec81537d9
JeanB788 <sandrob1109@gmail.com>
2026-03-05 19:10:05 -0600

    "Para tener algo en el repositorio"

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
+import tkinter as tk
+from tkinter import messagebox
+
+def saludar():
+    nombre = entrada_nombre.get()
+    if nombre:
+        messagebox.showinfo("Mensaje", f"Hola {nombre}, bienvenido a Tkinter")
+    else:
+        messagebox.showwarning("Advertencia", "Por favor, escribe tu nombre")
+
 if __name__ == '__main__':
-    print("Hola weones")
+
+    ventana = tk.Tk()
+    ventana.title("Mi Primera GUI")
+    ventana.geometry("300x200") # Ancho x Alto
+
+    etiqueta = tk.Label(ventana, text="Escribe tu nombre:", font=("Arial", 10))
+    entrada_nombre = tk.Entry(ventana)
+    boton_accion = tk.Button(ventana, text="Saludar", command=saludar)
+
+    # 3. Posicionar Widgets (Sistema de empaquetado)
+    etiqueta.pack(pady=10)
+    entrada_nombre.pack(pady=5)
+    boton_accion.pack(pady=20)
+
+    # 4. Iniciar el bucle de la aplicación
+    ventana.mainloop()
